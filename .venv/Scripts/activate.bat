9b212471d83c0892c1df51fe2bcd71e4f562e045
EswaraRajan <eswarrajan222@gmail.com>
2021-02-08 11:14:27 +0530

    initial commit

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Business\EnergyProject\apigateway\.venv
+set VIRTUAL_ENV=C:\wamp64\www\APIGateway-master\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
